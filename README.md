f82ec15dd796fb123de88da46d30eddf8cd9d627
Jonathan Murphy <MurphyWants@users.noreply.github.com>
2017-03-26 16:27:35 -0400

    Starting csv testing

--- README.md
+++ README.md
@@ -49,3 +49,6 @@ Code should be delivered in two ways:
 [ref-commit]: http://gitref.org/basic/#commit
 [ref-push]: http://gitref.org/remotes/#push
 [pull-request]: https://help.github.com/articles/creating-a-pull-request
+
+
+git@github.com:MurphyWants/FP3.git
--- fp.rkt
+++ fp.rkt
@@ -3,3 +3,17 @@
 (require csv-reading)
 
 (display "Hello World\n")
+
+; Copied from csv article
+
+(define make-food-csv-reader
+  (make-csv-reader-maker
+   '((separator-chars            #\|)
+     (strip-leading-whitespace?  . #t)
+     (strip-trailing-whitespace? . #t))))
+     
+(define next-row
+  (make-food-csv-reader (open-input-file "baseballdatabank-master/core/Schools.csv")))
+
+(next-row)
+(next-row)
