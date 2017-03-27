9057bcb9c4da2d3ea650df32e56e6188206effc1
goldenapplepie <skijg@yahoo.com>
2017-03-26 22:49:34 -0400

    Added program and screenshot
    Added racket file and screenshot of program in action.

---
+++ jennifer_green_fp3.rkt
@@ -0,0 +1,17 @@
+#lang racket
+(require racket/udp)
+
+(define socket1
+  (udp-open-socket ) )
+
+(define my-msg
+  (make-bytes 100) )
+
+(define (connect-to-port socket name port)
+  (udp-connect! socket name port) )
+
+(define (send-msg msg)
+  (udp-send socket1 msg) )
+
+(define (get-msg)
+  (udp-receive! socket1 my-msg) )
